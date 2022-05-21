591abeb7f00af3cd68de357818c7e21db39cc0a8
Veronika Rychlá <veronika.rychla108@gmail.com>
2022-05-21 23:06:52 +0200

    Gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../style_gallery.css';
+
+const panels = {
+  1: { imageUrl: '/gallery/background01.jpg', isActive: false },
+  2: { imageUrl: '/gallery/background02.jpg', isActive: false },
+  3: { imageUrl: '/gallery/background03.jpg', isActive: false },
+  4: { imageUrl: '/gallery/background04.jpg', isActive: false },
+  5: { imageUrl: '/gallery/background05.jpg', isActive: false },
+  6: { imageUrl: '/gallery/background06.jpg', isActive: false },
+};
 
 export const Gallery = () => {
+  const [gallery, setGallery] = useState(panels);
+
   return (
     <section>
       <div className="galerie bubble" id="galerie">
         <h2> Galerie</h2>
-        <button className="big-button">
-          <p>Look</p>
-        </button>
+        <div className="container">
+          {Object.keys(panels).map((index) => (
+            <div
+              className={gallery[index].isActive ? 'panel active' : 'panel'}
+              style={{ backgroundImage: `url(${gallery[index].imageUrl})` }}
+              key={index}
+              onClick={() => {
+                setGallery({
+                  ...panels,
+                  [index]: { imageUrl: panels[index].imageUrl, isActive: true },
+                });
+              }}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
