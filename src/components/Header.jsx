021b6b310ab16e26efc94a65fb516feabe2d3391
Veronika Rychlá <veronika.rychla108@gmail.com>
2022-05-21 19:57:21 +0200

    Prvni komponenty

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <section class="header">
+      <div class="header__text">
+        <div id="text__h1">
+          <h1>Japanglish</h1>
+        </div>
+        <div id="text__h4">
+          <h4>Trip to Tokyo</h4>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Navigation from './Navigation';
 
 const App = () => (
   <div className="container">
-    
+    <Header />
+    <Navigation />
+    <Outlet />
   </div>
 );
 
-createRoot(
-  document.querySelector('#app')
-).render(<App />);
+createRoot(document.querySelector('#app')).render(<App />);
